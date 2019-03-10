f04e06128a2e2bba71efc1ad973d882e9dd3f470
Janet <shi359@gmail.com>
2019-03-10 12:08:25 +0800

    add tmux

--- vim_plug.sh
+++ vim_plug.sh
@@ -1,3 +1,4 @@
+# vim plugin
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone https://github.com/sickill/vim-monokai/colors/monokai.vim ~/.vim/colors
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
